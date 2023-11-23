a6acffdec0637add60875cc2f67f311db83b7ac1
iloenzo99@gmail.com <iloenzo99@gmail.com>
2023-11-23 14:54:17 +0100

    finished version

--- index.php
+++ index.php
@@ -4,10 +4,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
-    <script src="inedx.js" defer></script>
+    <script src="index.js" defer></script>
     <title>Document</title>
 </head>
 <body>
-    
+    <canvas id="canvas"></canvas>
 </body>
 </html>
